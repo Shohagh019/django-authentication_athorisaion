97137cbbda6685eb97457cdb9a98091c0cad40d1
Shohagh019 <shohaghmunna@gmail.com>
2024-11-16 15:02:53 +0600

    password change added

--- first_app/urls.py
+++ first_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.user_profile, name='profile'),
+    path('change_password/', views.pass_change, name='pass_change'),
+    path('change_password_easy/', views.pass_change_easy, name='pass_change_easy'),
 ]
